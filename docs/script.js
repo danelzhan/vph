3436609f3a908ab4d2c37a260aa1cf5a0837a512
Sketchooo <117197566+enluia@users.noreply.github.com>
2024-03-28 16:52:32 -0700

    Prototype 1 site

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entries);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+hiddenElements.forEach((element) => observer.observe(element));
