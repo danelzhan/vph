6a7a21874665142abdcca1ecdb20a00f3b562e4a
Daniel Zhang <danielz4734@gmail.com>
2024-08-14 20:38:59 -0700

    gotta do staff page left

--- docs/script.js
+++ docs/script.js
@@ -9,3 +9,29 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((element) => observer.observe(element));
+
+//scroll
+var clickCount = 0;
+function pageScroll() {
+    if (clickCount < 2) {
+        window.scrollBy(0,5);
+        scrolldelay = setTimeout(pageScroll,1);
+    } else if (clickCount == 2) {
+        clickCount = 0;
+    }
+    document.onclick = function() {
+        clickCount++;
+    }
+}
+
+// buttons
+const registerButton = document.getElementById('volunteer_button');
+registerButton.addEventListener("click", function() {
+    window.location.href = 'volunteer_form.html';
+});
+
+const donateButton = document.getElementById('donate_button');
+registerButton.addEventListener("click", function() {
+    console.log('donated');
+    window.location.href = 'volunteer_form.html';
+});
