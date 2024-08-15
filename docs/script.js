c82c40997c7b07d06de55b500618bf7531199f63
Daniel Zhang <danielz4734@gmail.com>
2024-08-15 09:01:44 -0700

    pluh

--- docs/script.js
+++ docs/script.js
@@ -13,7 +13,7 @@ hiddenElements.forEach((element) => observer.observe(element));
 //scroll
 var clickCount = 0;
 function pageScroll() {
-    if (clickCount < 2) {
+    if (clickCount < 2) {   
         window.scrollBy(0,5);
         scrolldelay = setTimeout(pageScroll,1);
     } else if (clickCount == 2) {
@@ -34,4 +34,10 @@ const donateButton = document.getElementById('donate_button');
 registerButton.addEventListener("click", function() {
     console.log('donated');
     window.location.href = 'volunteer_form.html';
+});
+
+const contactButton = document.getElementById('sponsor_button');
+contactButton.addEventListener("click", function() {
+    console.log('contact');
+    window.location.href = 'contact.html';
 });
